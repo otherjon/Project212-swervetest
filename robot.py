df922b75fffe2d67571c98965e32fbee4a9ae801
Jon Solera <otherjon@emufarm.org>
2023-12-15 11:15:00 -0800

    robot.py: add some comments and documentation

--- robot.py
+++ robot.py
@@ -5,26 +5,59 @@ from typing import Optional
 import commands2
 import wpilib
 
+# The line below is some magic that we'll use to tell Python where to find
+# our container.py file.  (Of course, we need to perform this magic before we
+# attempt to import our container.py file!)
+#
 sys.path.append(str(Path(__file__).parent.parent.absolute()))
 
+# Now we can import the container module!
 from container import RobotContainer
 
 
 class Robot(commands2.TimedCommandRobot):
-    def robotInit(self):
+    """
+    A subclass of commands2.TimedCommandRobot which represents *our* robot.
+
+    Our robot defines most of its functionality in the RobotContainer class,
+    so this class just delegates most things to the RobotContainer and tells
+    the framework where to find its default things to do.
+    """
+    def robotInit(self) -> None:
+        """
+        When the robot software is initialized:
+         * make a RobotContainer (that's where our robot's behavior is defined)
+         * grab a copy of the CommandScheduler in case we need it later
+         * create a placeholder for a future autonomous command
+        """
         self.container = RobotContainer()
         self.scheduler = commands2.CommandScheduler.getInstance()
         self.autonomous_command: Optional[commands2.Command] = None
 
     def autonomousInit(self) -> None:
+        """
+        When we enter autonomous mode:
+         * get the autonomous command that our container says to use
+         * if the autonomous command isn't None, schedule it
+        """
         self.autonomous_command = self.container.get_autonomous_command()
         if self.autonomous_command:
             self.autonomous_command.schedule()
 
     def teleopInit(self) -> None:
+        """
+        When we enter teleoperated mode:
+         * if we were running an autonomous command, cancel it
+        """
         if self.autonomous_command:
             self.autonomous_command.cancel()
+            self.autonomous_command = None
 
 
+# The magical line below tells Python that if we're running the robot for real,
+# it should use the wpilib module's run() function to make our robot go.  (On
+# the other hand, if we're just importing this file into an interactive python
+# session, don't make the robot go.)
+#
 if __name__ == "__main__":
     wpilib.run(Robot)
