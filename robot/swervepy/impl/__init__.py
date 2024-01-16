384360aec7da0ef927afb12c53ebaf455713d452
Jon Solera <otherjon@emufarm.org>
2024-01-15 19:44:33 -0800

    copy in swervepy (see github.com/ewall25/swervepy)

---
+++ robot/swervepy/impl/__init__.py
@@ -0,0 +1,38 @@
+"""
+Contains default implementations of components (motors, sensors, modules). The user should instantiate these when
+creating their drive base.
+"""
+
+__all__ = [
+    "Falcon500CoaxialAzimuthComponent",
+    "Falcon500CoaxialDriveComponent",
+    "NEOCoaxialAzimuthComponent",
+    "NEOCoaxialDriveComponent",
+    "NEOCoaxialAzimuthComponent",
+    "AbsoluteCANCoder",
+    "AbsoluteDutyCycleEncoder",
+    "PigeonGyro",
+    "CoaxialSwerveModule",
+    "SparkMaxEncoderType",
+    "SparkMaxAbsoluteEncoder",
+    "DummyCoaxialComponent",
+    "DummyGyro",
+]
+
+from .motor import (
+    Falcon500CoaxialAzimuthComponent,
+    Falcon500CoaxialDriveComponent,
+    NEOCoaxialDriveComponent,
+    NEOCoaxialAzimuthComponent,
+    NEOCoaxialAzimuthComponent,
+    DummyCoaxialComponent,
+)
+from .sensor import (
+    AbsoluteCANCoder,
+    AbsoluteDutyCycleEncoder,
+    PigeonGyro,
+    SparkMaxEncoderType,
+    SparkMaxAbsoluteEncoder,
+    DummyGyro,
+)
+from .system import CoaxialSwerveModule
